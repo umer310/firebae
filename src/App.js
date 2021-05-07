aa3f1bf31e17a5d95d8c02544fc3302a8992cd7f
umer310 <umer.ataki@gmail.com>
2021-05-07 16:06:44 -0700

    before hooks

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
   getUserRepos = async (username) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users/${username}.repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRETS}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRETS}`
     );
     this.setState({ repos: res.data, loading: false });
   };
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -2,15 +2,20 @@ import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import Spinner from "../Spinner";
 import PropTypes from "prop-types";
+import Repos from "../repos/Repos";
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   };
   render() {
     const {
@@ -28,7 +33,7 @@ export class User extends Component {
       public_gists,
       hireable,
     } = this.props.user;
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
     if (loading) return <Spinner />;
     return (
       <Fragment>
@@ -96,6 +101,7 @@ export class User extends Component {
 
           <div className="badge badge-dark">Followers:{public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
